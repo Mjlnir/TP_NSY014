a066c8cf934a2f8a400ebaa79d4bc456992f59a6
ftrudell <florian.trudelle@gmail.com>
2021-07-04 11:03:02 +0200

    first commit

---
+++ RMI_Server/src/main/java/InformationImpl.java
@@ -0,0 +1,19 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class InformationImpl extends UnicastRemoteObject implements Information{
+	private String Message = "";
+	private static final long serialVersionUID = 2674880711467464646L;
+	protected InformationImpl() throws RemoteException{
+		super();
+	}
+	public void SendMessage(String message) throws RemoteException {
+		this.Message = message;
+	}
+	public String GetMessage() throws RemoteException {
+		String message = this.Message;
+		this.Message = "";
+		return message;
+	}
+	
+}
